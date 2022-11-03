d7901749c2913b7eab6041b0bb52edf7c285982f
icandle <39293311+icandle@users.noreply.github.com>
2022-11-03 22:44:24 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import os.path as osp
+
+import archs
+from basicsr.train import train_pipeline
+
+if __name__ == '__main__':
+    root_path = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir))
+    train_pipeline(root_path)
